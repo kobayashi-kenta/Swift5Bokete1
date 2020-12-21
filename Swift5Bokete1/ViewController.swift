054d6fdd1d63e59bb9e59a30ba003972fbc7b213
kobayashi-kenta <12321klj@gmail.com>
2020-12-21 22:19:06 +0900

    Initial Commit

---
+++ Swift5Bokete1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift5Bokete1
+//
+//  Created by Kenta on 2020/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
